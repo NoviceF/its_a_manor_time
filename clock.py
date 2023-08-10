e89e6d9422580d4b521ba5d7285645207fc3157e
Novice <NoviceF@users.noreply.github.com>
2023-08-10 22:03:14 +0300

    Update clock.py
    some improvements

--- clock.py
+++ clock.py
@@ -3,17 +3,20 @@
 import sys
 import time
 from datetime import datetime, timedelta
+import os
+import shutil
+
+os.system('clear')
+print('\n' * int(shutil.get_terminal_size().lines / 2))
 
 initTime = sys.argv[1:][0]
 
 hour, minutes, seconds = initTime.split(':')
 
 curTime = datetime.now()
-curTime = curTime.replace(hour=int(hour), minute=int(minutes), second=int(seconds))
+changedTime = curTime.replace(hour=int(hour), minute=int(minutes), second=int(seconds))
 
-oneSec = timedelta(seconds=1)
-
-newTime = curTime
+diff = changedTime - curTime
 
 class bcolors:
     HEADER = '\033[95m'
@@ -26,10 +29,31 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+warnTimeLow = curTime.replace(hour=int(20), minute=int(4), second=int(0))
+warnTimeLow2 = curTime.replace(hour=int(20), minute=int(5), second=int(40))
+warnTimeTop = curTime.replace(hour=int(20), minute=int(5), second=int(50))
+
+critTimeLow = curTime.replace(hour=int(20), minute=int(5), second=int(50))
+critTimeTop = curTime.replace(hour=int(20), minute=int(6), second=int(5))
+
 try:
 	while True:
-		print(newTime.strftime('\t{}%H:%M:%S{}'.format(bcolors.OKGREEN, bcolors.ENDC)), end='\r')
-		newTime = newTime + oneSec
+		newTime = datetime.now() + diff
+		color = bcolors.OKGREEN
+
+		if warnTimeLow <  newTime < warnTimeTop:
+			color = bcolors.WARNING
+			if warnTimeLow2 < newTime < warnTimeTop:
+				if int(newTime.strftime("%Y%m%d%H%M%S")) % 2:
+					color = bcolors.BOLD
+		elif critTimeLow < newTime < critTimeTop:
+			if int(newTime.strftime("%Y%m%d%H%M%S")) % 2:
+				color = bcolors.BOLD
+			else:
+				color = bcolors.FAIL
+
+		timeStr = newTime.strftime('\t{}%H:%M:%S{}'.format(color, bcolors.ENDC))
+		print(timeStr.center(shutil.get_terminal_size().columns), end='\r')
 		sys.stdout.flush()
 		time.sleep(1)
 except KeyboardInterrupt:
